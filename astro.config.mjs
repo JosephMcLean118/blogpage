1307d5e13d57bc3cb92eeb206d3e5df609bcc17f
JosephMclean118 <josephjmclean392@gmail.com>
2026-06-18 20:24:53 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://josephmclean118.github.io',
+  base: '/blogpage/',
+
   vite: {
     plugins: [tailwindcss()]
   }
